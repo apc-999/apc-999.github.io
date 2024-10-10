54bfc188c36e380710b84283d967df91987b83f9
apc999 <anhcowen@amazon.co.uk>
2024-10-10 22:28:10 +0100

    Delete confirmation

--- src/main.py
+++ src/main.py
@@ -10,13 +10,10 @@ from werkzeug.utils import secure_filename
 import hashlib
 app = Flask(__name__, template_folder='../templates', static_folder='../resources')
 app.secret_key = 'QAsucks'
-
 resources=os.path.join(app.root_path, os.pardir)+"/resources/"
 app.config['UPLOAD_FOLDER'] = resources+'static/images'
-
 if not os.path.exists(app.config['UPLOAD_FOLDER'] ):
     os.makedirs(app.config['UPLOAD_FOLDER'])
-print(resources)
 db_path = resources+"data.db"
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
@@ -45,7 +42,6 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS "users" (
 conn.commit()
 conn.close()
 
-# Password hashing
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
@@ -71,7 +67,6 @@ def error_message(error=None):
     return msg
 def insert_data(row):
     with get_db() as db:
-        print(row)
         cursor = db.cursor()
         cursor.execute('''
         INSERT INTO data (ShortId, Title, Severity, Status, AssignedGroup, AssigneeIdentity, CreateDate, LastUpdatedDate, "Issue open", "Root cause")
@@ -100,7 +95,6 @@ def update_row(short_id, column, new_value):
     with get_db() as db:
         cursor = db.cursor()
         query = f'''UPDATE data SET "{column}" = ? WHERE ShortId = ?'''
-        print(query)
         cursor.execute(query, (new_value, short_id))
         db.commit()
     print(f"Ticket ShortId {short_id} has been updated.")
@@ -123,21 +117,17 @@ def add_issue():
     if request.method == 'POST':
         rowtowrite = []
         headers = get_headers()
-        
-        # Process form data
         for header in headers:
             value=request.form.get(header)
             if header == 'CreateDate' or header == 'LastUpdatedDate':
                 if header=="LastUpdatedDate":
                     value=current_datetime()
                 elif not value:
-                    print("there's an error at Date")
                     error = error_message(error=f'{header} is required.')
                     break
                 try:
                     value = getDateTimeFromISO8601String(value).replace(tzinfo=None).isoformat()+"Z"
                 except:
-                    print("there's an error at ISO Date")
                     error = error_message(error="Please enter a valid date in ISO8601 format.")
                     break
                 rowtowrite.append(value)
@@ -150,48 +140,38 @@ def add_issue():
                     else:
                         raise ValueError
                 except:
-                    print("there's an error at sev")
                     error = error_message(error="Severity must be a SEV number between 1-5")
                     failed=True
                     break
                 rowtowrite.append(value)
                 
             elif header == 'Issue open':
-                print(value)
                 issue_open = value == 'on'
                 rowtowrite.append(str(issue_open).upper())
                 
             elif header == 'Status':
                 valid_statuses = ["Assigned", "Researching", "Work in Progress", "Resolved"]
                 if value not in valid_statuses:
-                    print("there's an error at status")
                     error = error_message(error=f'Status must be one of {valid_statuses}')
                     failed=True
                     break
                 rowtowrite.append(value)
             elif header == 'Root cause':
-                print(rowtowrite[-1])
-                if not value and str(rowtowrite[-1]).upper() == "FALSE":  # Check if the issue is open
+                if not value and str(rowtowrite[-1]).upper() == "FALSE":
                     error = error_message( error=f'Root Cause is required if the issue is open.')
                     break
-                rowtowrite.append(value)  # Append even if it's empty
+                rowtowrite.append(value)
             else:
-                print(header,value)
                 if not value:
-                    print("there's an error at not value")
                     error = error_message(error=f'{header} is required.')
                     break
                 rowtowrite.append(value)
-
-        # Insert the new row into the database
         if error:
-            print(error)
             return render_template('add_issue.html', error=error)
         else:
             insert_data(rowtowrite)
             return redirect(url_for('show_issues'))
     else:
-    # For GET requests, show the form
         return render_template('add_issue.html')
 @app.route('/delete_issue', methods=['GET', 'POST'])
 def delete_issue():
@@ -201,11 +181,10 @@ def delete_issue():
     elif get_user_info(session['user_id'],3) != "admin":
         return redirect(url_for('show_issues',error=error_message("Must be an admin to access that page")))
     if request.method == 'POST':
-        # Get the ticket ID from the form submission
         ticket_id = request.form.get('ticket_id')
         if ticket_id:
-            delete_row(ticket_id)  # Call your function to delete the row
-            return redirect(url_for('delete_issue'))  # Redirect to the delete page after deletion
+            delete_row(ticket_id)
+            return redirect(url_for('delete_issue'))
 
     # For GET requests, display all rows
     all_rows = fetch_data()
@@ -219,34 +198,29 @@ def resolve_issue():
     all_rows = fetch_data()
     headers = get_headers()
 
-    unresolved_issues = [row for row in all_rows if row[8] == "TRUE"]  # Assuming the status is in column 8
+    unresolved_issues = [row for row in all_rows if row[8] == "TRUE"]
     current_root_cause = None
 
     if request.method == 'POST':
-        # Handle the form submission to resolve an issue
         ticket_id = request.form.get('ticket_id')
         root_cause = request.form.get('root_cause', '').strip()
     
 
         for row in unresolved_issues:
-            if row[0] == ticket_id:  # Adjust based on your ticket identifier
-                current_root_cause = row['Root cause']  # Use the actual column 
-        print(current_root_cause)
-        # If there's no current root cause, require a new one
+            if row[0] == ticket_id:
+                current_root_cause = row['Root cause'] 
         if not current_root_cause and not root_cause:
             return render_template('resolve_issue.html', 
                                    unresolved_issues=unresolved_issues, 
                                    headers=headers, 
                                    error="Root cause is required to resolve this issue.",
                                    current_root_cause=current_root_cause)
-        print(ticket_id)
         update_row(ticket_id, 'Issue open', "FALSE")
         update_row(ticket_id, "LastUpdatedDate", current_datetime())
         if root_cause: 
             update_row(ticket_id, 'Root cause', root_cause)
 
-        return redirect(url_for('resolve_issue'))  # Redirect to the same page to avoid resubmission
-
+        return redirect(url_for('resolve_issue'))
     if not unresolved_issues:
         print("There are no issues to resolve, please add some.")
         error=error_message("No issues to resolve.")
@@ -262,11 +236,9 @@ def update_issue():
     
     db = get_db()
     cursor = db.cursor()
-    ticket_id = request.args.get('ticket_id')  # Get the ticket ID from the URL
-
+    ticket_id = request.args.get('ticket_id')
     if not ticket_id:
-        # If no ticket_id is provided, fetch all issues and render the selection page
-        all_rows = fetch_data()  # Fetch all rows
+        all_rows = fetch_data()
         headers = get_headers()
 
         if not all_rows:
@@ -275,34 +247,28 @@ def update_issue():
         return render_template('select_issue.html', issues=all_rows, headers=headers)
     
     if request.method == 'POST':
-        # This block handles form submission for updating the issue
         updated_data = {}
         headers = get_headers()
         
-        # Collect form data
         for header in headers:
             updated_data[header] = request.form.get(header)
         
-        # Update each field in the database where there was a change
         for header, new_value in updated_data.items():
-            if new_value and new_value.strip():  # Ensure no empty values
+            if new_value and new_value.strip(): 
                 update_row(ticket_id, header, new_value)
         
-        # After updating, redirect to show_issues or confirmation page
-        return redirect(url_for('show_issues'))  # or another success page
+        return redirect(url_for('show_issues')) 
 
     else:
-        # For a GET request, fetch and display the issue's details in the form
         query = "SELECT * FROM data WHERE ShortId = ?"
         cursor.execute(query, (ticket_id,))
         issue = cursor.fetchone()
 
         if not issue:
-            return "Issue not found", 404  # Handle case where the ticket doesn't exist
+            return "Issue not found", 404
         
         headers = get_headers()
         
-        # Render the form pre-filled with the issue's current details
         return render_template('update_issue.html', issue=issue, headers=headers)
 
 
@@ -312,10 +278,10 @@ def find_issue():
         return redirect(url_for('show_issues'))
     db = get_db()
     cursor = db.cursor()
-    headers = get_headers()  # Assumes you have a function to get column headers
-    matching_rows = []  # Initialize empty list to hold results
+    headers = get_headers()  
+    matching_rows = []
 
-    if request.method == 'POST':  # When the search form is submitted
+    if request.method == 'POST':
         search_term = request.form.get('search_term')
         query = '''SELECT * FROM data 
                    WHERE ShortId LIKE ? OR Title LIKE ? OR Severity LIKE ? 
@@ -330,7 +296,6 @@ def find_issue():
 
     return render_template('find_issue.html', headers=headers, matching_rows=matching_rows)
 
-# Sign-up route
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     error=request.args.get('error')
@@ -340,7 +305,6 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         
-        # Hash the password
         hashed_password = hash_password(password)
         
         db = get_db()
@@ -356,7 +320,6 @@ def signup():
     
     return render_template('signup.html',error=error)
 
-# Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error=request.args.get('error')
@@ -386,7 +349,7 @@ def login():
 @app.route('/change_password', methods=['GET', 'POST'])
 def change_password():
     if 'user_id' not in session:
-        return redirect(url_for('login'))  # Ensure only logged-in users can access this
+        return redirect(url_for('login'))
 
     user_id = session['user_id']
 
@@ -400,10 +363,9 @@ def change_password():
         db.commit()
         db.close()
 
-        return redirect(url_for('menu'))  # Redirect to the main menu after changing password
+        return redirect(url_for('menu'))
 
-    return render_template('change_password.html')  # Render a template for changing the password
-
+    return render_template('change_password.html')
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     error=request.args.get('error')
@@ -425,8 +387,6 @@ def admin():
             cursor.execute('UPDATE users SET "profile-img" = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" WHERE id = ?', (user_id))
         else:
             if profile_image:
-                print("yes")
-                # Save the profile image and update the path in the database
                 filename = secure_filename(profile_image.filename)
                 full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 profile_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -440,7 +400,6 @@ def admin():
         db.close()
         return redirect(url_for('admin', error="Success"))
 
-    # Fetch all users for display
     cursor.execute('SELECT * FROM users')
     users = cursor.fetchall()
     
